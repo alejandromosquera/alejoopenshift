08f024cf16f25e2102d7c436fa727bd173cd5e22
sala_c <sala_c@10.180.12.42>
2016-11-05 11:33:29 -0500

    JKeje

--- src/main/java/example/microservice/micro.java
+++ src/main/java/example/microservice/micro.java
@@ -1,5 +1,8 @@
 package example.microservice;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.jws.WebService;
@@ -17,5 +20,27 @@ public class micro {
 	public int sumar(@WebParam(name="valor1") int a,@WebParam(name="valor2") int b) {
 	    System.out.println("sumar: " + (a+b));
 	    return (a+b);
+	}
+	
+	@WebMethod()
+	public List<Placa> lista_placas_robadas() {
+		List<Placa> val = new ArrayList<>();
+		
+		Placa p1 = new Placa();
+		p1.setPlaca("Placa1");
+		p1.setPropiedad("Juan");		
+		val.add(p1);
+	
+		Placa p2 = new Placa();
+		p2.setPlaca("Placa2");
+		p2.setPropiedad("Alejandro");		
+		val.add(p2);
+		
+	    return val;
+	}
+	
+	@WebMethod()
+	public boolean verificar_placa(@WebParam(name="numero_placa") String name) {	    
+	    return true;
 	}
 }
